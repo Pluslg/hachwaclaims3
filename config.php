e9b4294e145021eab59df0bc3b3949095879dff6
Pluslg <59405367+Pluslg@users.noreply.github.com>
2020-03-08 21:02:40 -0300

    Add files via upload
    jfifighfgdyd

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$email = "josedenisonsf@gmail.com";
+$address = "32nWWgAHXFi2McpjYPFYVeRqHL7zXYvgeH";
+
+/* Link Pass :    https://semawur.com/0dGrBL7wm0   */
+
+$pass_scrip = "hhhskej";
+
+
+
